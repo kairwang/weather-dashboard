f8e9db91733306bff02b8fc3cb7362f760ccab86
kairwang <kaiwang246@gmail.com>
2024-07-11 01:40:17 -0400

    Update website: Features and Styling

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const app = express();
 const port = 3000;
 const API_URL = "https://api.openweathermap.org/data/2.5";
-const API_KEY = "63a8f777ff82342a3326056f641f6604"; // Replace with your API key
+const API_KEY = "63a8f777ff82342a3326056f641f6604";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -35,14 +35,23 @@ app.post("/get-weather", async (req, res) => {
             params: {
                 q: city,
                 appid: API_KEY,
-                units: 'metric'
+                units: 'imperial'
             }
         });
 
         // Add city to search history
         searchHistory.push(city);
 
-        res.render("index.ejs", { weather: [{ city: result.data.name, temperature: result.data.main.temp, description: result.data.weather[0].description }], error: null, history: searchHistory });
+        res.render('index.ejs', {
+            weather: [{
+                city: result.data.name,
+                temperature: result.data.main.temp,
+                description: result.data.weather[0].description,
+                icon: result.data.weather[0].icon,
+            }],
+            error: null,
+            history: searchHistory,
+        });
     } catch (error) {
         console.error(error);
         res.render("index.ejs", { weather: [], error: "Could not fetch weather data", history: searchHistory });
@@ -65,15 +74,20 @@ app.get("/current-location", async (req, res) => {
             params: {
                 q: city,
                 appid: API_KEY,
-                units: "metric",
+                units: "imperial",
             },
         });
 
         // Add city to search history
         searchHistory.push(city);
 
-        res.render("index.ejs", {
-            weather: [{ city: weatherResponse.data.name, temperature: weatherResponse.data.main.temp, description: weatherResponse.data.weather[0].description }],
+        res.render('index.ejs', {
+            weather: [{
+                city: weatherResponse.data.name,
+                temperature: weatherResponse.data.main.temp,
+                description: weatherResponse.data.weather[0].description,
+                icon: weatherResponse.data.weather[0].icon,
+            }],
             error: null,
             history: searchHistory,
         });
